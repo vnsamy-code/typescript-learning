0eeb42f8d416f646d013fe32c5f2c829f7b72adc
Vignesh, N <101890317+vnsamy-code@users.noreply.github.com>
2022-05-31 20:34:20 +0200

    Add typeof operator example.

--- tmp/playground.js
+++ tmp/playground.js
@@ -6,5 +6,12 @@ var contact = {
     id: 90,
     name: 'Cavin'
 };
-var field = getFieldValue(contact, "name");
+var field = getFieldValue(contact, "id");
 console.log("Name field value is: ".concat(field));
+// typeof operator example
+var range = { min: 1, max: 100 };
+function printRange(params) {
+    console.log("min from: ".concat(params.min, " and max to: ").concat(params.max));
+}
+// ❌ getRange({ x: 1000, y: 1000 }); - This won't work since the property names are different.
+printRange({ min: 900, max: 1000 });
--- tmp/playground.ts
+++ tmp/playground.ts
@@ -1,5 +1,7 @@
 console.log('it works...');
 
+// keyof operator example
+
 interface Person {
     name: string
 }
@@ -24,4 +26,15 @@ const contact: Contact = {
 
 const field = getFieldValue(contact, "id");
 
-console.log(`Name field value is: ${field}`)
+console.log(`Name field value is: ${field}`);
+
+// typeof operator example
+
+const range = { min: 1, max: 100 };
+
+function printRange(params: typeof range) {
+    console.log(`min from: ${params.min} and max to: ${params.max}`);
+}
+
+// ❌ getRange({ x: 1000, y: 1000 }); - This won't work since the property names are different.
+printRange({ min: 900, max: 1000 });
